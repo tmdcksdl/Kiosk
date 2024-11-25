868f800380004c7a68cb543cd277c101819184de
tmdcksdl <tmdcksdl99@naver.com>
2024-11-25 15:22:17 +0900

    Main class : Fix print menus

--- src/kiosk/Main.java
+++ src/kiosk/Main.java
@@ -26,10 +26,7 @@ public class Main {
         while (true) {
             // 안내 문구 출력
             System.out.println("[ SHAKESHACK MENU ]");
-            System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-            System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-            System.out.println("3. CheeseBurger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-            System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+            menu.printMenu();
             System.out.println("0. 종료           | 종료");
             // 사용자로부터 정수형 값을 입력받아 변수 number에 저장.
             int number = scanner.nextInt();

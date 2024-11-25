f1bf058a0d752d4810f260a1095c0af837644d13
tmdcksdl <tmdcksdl99@naver.com>
2024-11-25 12:00:06 +0900

    MenuItem Class : Add fields and constructor

--- src/MenuItem.java
+++ src/MenuItem.java
@@ -1,11 +1,20 @@
 public class MenuItem {
 
     // 1. 속성 (필드)
+    // 이름, 가격, 설명
 
+    // 이름
+    String burgerName;
+    // 가격
+    Double burgerPrice;
+    // 설명
+    String burgerComment;
 
     // 2. 생성자 (조립설명서)
-    public MenuItem () {
-
+    public MenuItem (String burgerName, Double burgerPrice, String burgerComment) {
+        this.burgerName = burgerName;
+        this.burgerPrice = burgerPrice;
+        this.burgerComment = burgerComment;
     }
 
     // 3. 기능 (메서드)

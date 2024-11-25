7e343de3cde8e7ac270e1738e0df7447ead9656b
tmdcksdl <tmdcksdl99@naver.com>
2024-11-25 14:18:23 +0900

    Menu class : Create Menu class

---
+++ src/Menu.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Menu {
+
+    // 1. 속성 (필드)
+    // 메뉴 목록(menus) : ArrayList
+    private ArrayList<MenuItem> menus;
+
+    // 2. 생성자 (조립설명서)
+    public Menu () {
+        menus = new ArrayList<>();
+    }
+
+
+    // 3. 기능 (메서드)
+}

fd5bbb23fb74a6897824096fa9d75d82ba7f401d
tmdcksdl <tmdcksdl99@naver.com>
2024-11-25 15:21:51 +0900

    Menu class : Create addItems Method, printMenu Method

--- src/kiosk/Menu.java
+++ src/kiosk/Menu.java
@@ -13,9 +13,23 @@ public class Menu {
         this.menus = new ArrayList<>();
     }
 
-
     // 3. 기능 (메서드)
+    /**
+     * 메뉴 리스트에 아이템을 추가하는 메서드
+     * @param menuItem
+     */
     public void addItems (MenuItem menuItem) {
         this.menus.add(menuItem);
+    }
+
+    /**
+     * 반복문을 통해 메뉴 리스트 안에 있는 item들 하나씩 출력하는 메서드
+     */
+    public void printMenu () {
+        int i = 1;
+        for (MenuItem menuItem : menus) {
+            System.out.println(i + ". " + menuItem.getBurgerName() + "   | W " + menuItem.getBurgerPrice() + " | " + menuItem.getBurgerComment());
+            i++;
+        }
     }
 }

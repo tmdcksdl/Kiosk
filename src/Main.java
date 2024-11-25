738b12510b97dbb6bd83eb5148c238986e844616
tmdcksdl <tmdcksdl99@naver.com>
2024-11-25 13:59:54 +0900

    Main class : Create Hamburgers

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // 사용자로부터 입력을 받기 위해 Scanner 객체 생성.
         Scanner scanner = new Scanner(System.in);
+        // 햄버거 메뉴를 저장할 리스트를 생성.
+        List<MenuItem> menuItems = new ArrayList<>();
+
+        // 햄버거 객체 생성
+        MenuItem hamburger1 = new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+        MenuItem hamburger2 = new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+        MenuItem hamburger3 = new MenuItem("CheeseBurger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+        MenuItem hamburger4 = new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거");
 
         // while문 실행.
         while (true) {

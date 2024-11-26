e48f0a9dff45f9afe34c1d3e350bcc5eb78cd73c
tmdcksdl <tmdcksdl99@naver.com>
2024-11-26 10:45:36 +0900

    Main class : Delete import

--- src/kiosk/Main.java
+++ src/kiosk/Main.java
@@ -1,9 +1,5 @@
 package kiosk;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Kiosk kiosk = new Kiosk();

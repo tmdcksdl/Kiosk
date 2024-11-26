32b232b784b094ea92154fecf1e5f688a4a177b6
tmdcksdl <tmdcksdl99@naver.com>
2024-11-26 11:08:37 +0900

    Menu class : Create printHamburger Method

--- src/kiosk/Menu.java
+++ src/kiosk/Menu.java
@@ -6,13 +6,26 @@ public class Menu {
 
     // 1. 속성 (필드)
     // 메뉴 목록(menus) : ArrayList
-
+    ArrayList<MenuItem> menuItems;
 
     // 2. 생성자 (조립설명서)
     public Menu () {
-
+        this.menuItems = new ArrayList<>();
     }
 
     // 3. 기능 (메서드)
-
+    public void printHamburger (ArrayList<MenuItem> menuItems) {
+        int i = 1;
+        for (MenuItem menu : menuItems) {
+            System.out.println(
+                    String.format(
+                            "%-2d. %-15s | W %-6.2f | %s",
+                            i++,                            // 번호
+                            menu.getBurgerName(),       // 버거 이름 (15자 너비)
+                            menu.getBurgerPrice(),      // 버거 가격 (6자리 너비)
+                            menu.getBurgerComment()
+                    )
+            );
+        }
+    }
 }

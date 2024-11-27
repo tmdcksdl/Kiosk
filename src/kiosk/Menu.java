b2f0cb2739035b5b031f93645aa2012c8601c2cd
tmdcksdl <tmdcksdl99@naver.com>
2024-11-27 14:24:22 +0900

    Menu class : Create menuDrinks List

--- src/kiosk/Menu.java
+++ src/kiosk/Menu.java
@@ -8,23 +8,77 @@ public class Menu {
     // 햄버거 목록(menuHamburgers) : ArrayList
     private ArrayList<MenuItem> menuHamburgers;
 
+    // 음료 목록(menuDrinks) : ArrayList
+    private ArrayList<MenuItem> menuDrinks;
+
+    // 디저트 목록(menuDesserts) : ArrayList
+    private ArrayList<MenuItem> menuDesserts;
+
     // 2. 생성자 (조립설명서)
-    public Menu () {
+    public Menu() {
         this.menuHamburgers = new ArrayList<>();
+        this.menuDrinks = new ArrayList<>();
+        this.menuDesserts = new ArrayList<>();
     }
 
     // 3. 기능 (메서드)
-    public void addHamburger (MenuItem menuItem) {
+    public void addHamburger(MenuItem menuItem) {
         this.menuHamburgers.add(menuItem);
     }
 
-    public ArrayList<MenuItem> getHamburgerItems () {
+    public void addDrink(MenuItem menuItem) {
+        this.menuDrinks.add(menuItem);
+    }
+
+    public void addDessert(MenuItem menuItem) {
+        this.menuDesserts.add(menuItem);
+    }
+
+    public ArrayList<MenuItem> getHamburgerItems() {
         return this.menuHamburgers;
     }
 
-    public void printHamburger () {
+    public ArrayList<MenuItem> getDrinkItems() {
+        return this.menuDrinks;
+    }
+
+    public ArrayList<MenuItem> getDessertItems() {
+        return this.menuDesserts;
+    }
+
+    public void printHamburger() {
         int i = 1;
         for (MenuItem menu : menuHamburgers) {
+            System.out.println(
+                    String.format(
+                            "%-1d. %-15s | W %-3.1f | %s",
+                            i++,                            // 번호
+                            menu.getItemName(),       // 버거 이름 (15자 너비)
+                            menu.getItemPrice(),      // 버거 가격 (6자리 너비)
+                            menu.getItemComment()
+                    )
+            );
+        }
+    }
+
+    public void printDrink() {
+        int i = 1;
+        for (MenuItem menu : menuDrinks) {
+            System.out.println(
+                    String.format(
+                            "%-1d. %-15s | W %-3.1f | %s",
+                            i++,                            // 번호
+                            menu.getItemName(),       // 버거 이름 (15자 너비)
+                            menu.getItemPrice(),      // 버거 가격 (6자리 너비)
+                            menu.getItemComment()
+                    )
+            );
+        }
+    }
+
+    public void printDessert() {
+        int i = 1;
+        for (MenuItem menu : menuDesserts) {
             System.out.println(
                     String.format(
                             "%-1d. %-15s | W %-3.1f | %s",

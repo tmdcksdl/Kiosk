866c3de52d3fb373e875c21dbb7f4958990da3bd
tmdcksdl <tmdcksdl99@naver.com>
2024-11-27 13:45:31 +0900

    MenuItem class : Change Field, Method name

--- src/kiosk/MenuItem.java
+++ src/kiosk/MenuItem.java
@@ -6,17 +6,17 @@ public class MenuItem {
     // 이름, 가격, 설명
 
     // 이름
-    private String burgerName;
+    private String itemName;
     // 가격
-    private double burgerPrice;
+    private double itemPrice;
     // 설명
-    private String burgerComment;
+    private String itemComment;
 
     // 2. 생성자 (조립설명서)
-    public MenuItem (String burgerName, double burgerPrice, String burgerComment) {
-        this.burgerName = burgerName;
-        this.burgerPrice = burgerPrice;
-        this.burgerComment = burgerComment;
+    public MenuItem (String itemName, double itemPrice, String itemComment) {
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.itemComment = itemComment;
     }
 
     // 3. 기능 (메서드)
@@ -24,23 +24,23 @@ public class MenuItem {
      * 햄버거 이름을 가져오는 메서드
      * @return 햄버거 이름(burgerName)
      */
-    public String getBurgerName () {
-        return this.burgerName;
+    public String getItemName () {
+        return this.itemName;
     }
 
     /**
      * 햄버거 가격을 가져오는 메서드
      * @return 햄버거 가격(burgerPrice)
      */
-    public double getBurgerPrice () {
-        return this.burgerPrice;
+    public double getItemPrice () {
+        return this.itemPrice;
     }
 
     /**
      * 햄버거 설명을 가져오는 메서드
      * @return 햄버거 설명(burgerComment)
      */
-    public String getBurgerComment () {
-        return this.burgerComment;
+    public String getItemComment () {
+        return this.itemComment;
     }
 }
